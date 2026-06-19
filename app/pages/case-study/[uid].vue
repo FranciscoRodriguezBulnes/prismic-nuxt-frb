775f09b7a02bfb6f6239850cbfb1dc3d372f92b8
Francisco Rodríguez Bulnes <158209260+FranciscoRodriguezBulnes@users.noreply.github.com>
2026-06-19 12:11:06 +0200

    Last Updates

--- app/pages/case-study/[uid].vue
+++ app/pages/case-study/[uid].vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import gsap from "gsap";
 import { asImageSrc } from "@prismicio/client";
+import Bounded from "~/components/Bounded.vue";
 import { components } from "~/slices";
 
 const route = useRoute();
@@ -7,16 +9,58 @@ const { client } = usePrismic();
 const { data: page } = await useAsyncData(`[case_study-uid-${route.params.uid}]`, () => client.getByUID("case_study", route.params.uid as string));
 
 useSeoMeta({
-  title: page.value?.data.meta_title,
-  ogTitle: page.value?.data.meta_title,
-  description: page.value?.data.meta_description,
-  ogDescription: page.value?.data.meta_description,
+  title: page.value?.data.meta_title ?? undefined,
+  ogTitle: page.value?.data.meta_title ?? undefined,
+  description: page.value?.data.meta_description ?? undefined,
+  ogDescription: page.value?.data.meta_description ?? undefined,
   ogImage: computed(() => asImageSrc(page.value?.data.meta_image)),
+});
+
+onMounted(() => {
+  const prefersReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce").matches;
+
+  if (prefersReducedMotion) return;
+
+  // const tl = gsap.timeline({
+  //   repeat: -1,
+  //   defaults: { ease: "power2.inOut" },
+  // });
+
+  gsap.fromTo(
+    ".caseStudy__image",
+    {
+      opacity: 0,
+      y: 100,
+    },
+    {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      delay: 0.5,
+      ease: "power2.inOut",
+    },
+  );
 });
 </script>
 
 <template>
   <main>
-    <SliceZone :slices="page?.data.slices ?? []" :components="components" />
+    <Bounded>
+      <header class="relative grid w-full place-items-center text-center">
+        <GlideGrid />
+        <h1 class="text-5xl font-medium text-balance text-center md:text-7xl">
+          <PrismicText :field="page?.data.company" />
+          <RichTextGlideText class="block text-lg">Case Study</RichTextGlideText>
+        </h1>
+        <p class="mb-4 mt-8 max-w-xl text-lg text-gray-300">
+          <PrismicText :field="page?.data.description" />
+        </p>
+        <PrismicImage v-if="$prismic.isFilled.image(page?.data.cover)" :field="page?.data.cover" class="caseStudy__image rounded-lg max-w-4xl w-full" />
+      </header>
+    </Bounded>
+
+    <di class="mx-auto mt-12 md:mt-16">
+      <SliceZone :slices="page?.data.slices ?? []" :components="components" />
+    </di>
   </main>
 </template>
--- app/slices/RichText/index.vue
+++ app/slices/RichText/index.vue
@@ -1,25 +1,15 @@
 <script setup lang="ts">
 import type { Content } from "@prismicio/client";
 
-defineProps(getSliceComponentProps<Content.RichTextSlice>());
+// The array passed to \`getSliceComponentProps\` is purely optional.
+// Consider it as a visual hint for you when templating your slice.
+defineProps(getSliceComponentProps<Content.RichTextSlice>(["slice", "index", "slices", "context"]));
 </script>
 
 <template>
-  <section>
-    <PrismicRichText :field="slice.primary.content" />
-  </section>
+  <Bounded>
+    <div class="prose prose-lg prose-slate prose-invert w-full">
+      <PrismicRichText :field="slice.primary.content" />
+    </div>
+  </Bounded>
 </template>
-
-<style scoped>
-section {
-  max-width: 600px;
-  margin: 6em auto;
-  /* font-family:
-    -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial,
-    sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"; */
-}
-
-section:deep(.codespan) {
-  font-family: monospace;
-}
-</style>
